25c6aed870ae2f1397c42fba3050af8cff0adcd2
Jefferson Rhudson <jrhudson@ciandt.com>
2022-07-27 10:57:30 -0300

    remover o social login já que n presta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider
+} from 'angularx-social-login';
+import { GoogleSigninButtonDirectiveDirective } from './google-signin-button-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleSigninButtonDirectiveDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '749824175933-egjopdpm4n3b41rb05t1l0rjvhbqbe1o.apps.googleusercontent.com'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
