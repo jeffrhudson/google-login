41a49196762bbbbb1b5205ec7cac3b053af877a4
Jefferson Rhudson <jrhudson@ciandt.com>
2022-07-27 14:16:19 -0300

    auth simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 
 declare let google: any;
 
 @Component({
   selector: 'app-root',
   template: `
-    <!-- <div
+    <div
     id="g_id_onload"
     data-client_id="749824175933-egjopdpm4n3b41rb05t1l0rjvhbqbe1o.apps.googleusercontent.com"
     data-cancel_on_tap_outside="false"
-    (data-callback)="response">
-  </div> -->
-    <button asl-google-signin-button>login</button>
+    data-callback="handleCredentialResponse">
+    </div>
   `,
   styles: []
 })
 export class AppComponent implements OnInit{
   title = 'google-login';
 
-  constructor(private authService: SocialAuthService){}
+  constructor(){}
 
   ngOnInit(): void {
     
